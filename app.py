92c8735e802185cd557d80221545ef7f1ce38f55
vin-ren <65110648+vin-ren@users.noreply.github.com>
2022-02-23 17:26:19 +0900

    Fix bugs, Restructure, Collected Endpoint Added
    Fixes some minor bugs.
    Restructured app.py, moved input endpoint to inside of the API, added show_collected endpoint and its copy to clipboard content, also removed atexit.
    
    renamed some stuff, added placeholder for status and response alerts, commented unnecesary script imports, added copy to clipboard function, added header field.
    
    Added a little bit of styling for table, and buttons.
    
    Restructured index.html, changed the onclick and how the submission works.
    
    Added feature to disable status button.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import atexit
+from faulthandler import disable
 import json
 from datetime import datetime
 from flask import Blueprint, jsonify, render_template, url_for, request
@@ -15,25 +15,25 @@ class objectifiedDict(dict):
     def __setattr__(self, name, value):
         return super().__setitem__(name, value)
 
-env = objectifiedDict(time_format='%H:%M')
+
+env = objectifiedDict(time_format='%H:%M', cacheFile='cached.json')
 attendanceList = {}
-
-
-def loadCached():
-    try:
-        with open('cached.json', 'r') as f:
-            return json.load(f)
-    except:
-        pass
 
 
 attendance = Blueprint('attendance', __name__, url_prefix='/attendance')
 
 
+def loadCached():
+    try:
+        with open(env.cacheFile, 'r') as f:
+            return json.load(f)
+    except:
+        pass
+
+
 def saveAttendance():
-    with open('cached.json', 'w') as f:
+    with open(env.cacheFile, 'w') as f:
         json.dump(attendanceList, f, indent=2)
-atexit.register(saveAttendance)
 
 def timeParser(s:str):
     formats = ['%H:%M', '%H:%M:%S', '%d %b %H:%M', '%H %M', '%H %M %S', '%H']
@@ -45,45 +45,67 @@ def timeParser(s:str):
             pass
     return datetime.fromtimestamp(int(s))
 
-@attendance.route('/input', methods=['POST'])
-def inputAPI():
-    try:
-        data = dict(request.form)
-        # print(data)
-        deltaTimeFromLimit = (env.time_limit - datetime.now())
-        if not data['name']:
-            raise KeyError
-        if deltaTimeFromLimit.total_seconds() <= 0:
-            # lateness = datetime.fromtimestamp(abs(deltaTimeFromLimit.total_seconds()))
-            
-            return jsonify({'success': False, 'reason':'Batas Waktu Telah Dilampaui.'})
-        attendanceList[data['name']] = data['status']
-        saveAttendance()
-    except KeyError:
-        return jsonify({'success': False, 'reason':'Tolong Masukan Nama Anda'})
-    except Exception as exc:
-        return jsonify({'success': False, 'reason':'Exception Caught', 'exception': [exc.__class__.__name__, exc.args]})
-    return jsonify({'success':True, 'success_message':'Kehadiranmu Telah Dicatat Sebagai {}.'.format(data['status'])})
+@attendance.route('/API/<string:endpoint>', methods=['GET', 'POST'])
+def API(endpoint):
+    if endpoint.lower() == 'input':
+        try:
+            data = dict(request.form)
+            # print(data)
+            deltaTimeFromLimit = (env.time_limit - datetime.now())
+            if not data['name']:
+                raise KeyError
+            if deltaTimeFromLimit.total_seconds() <= 0:
+                # lateness = datetime.fromtimestamp(abs(deltaTimeFromLimit.total_seconds()))
+                return jsonify({'success': False, 'reason':'Batas Waktu Telah Dilampaui.'})
+            attendanceList[data['name']] = data['status']
+            saveAttendance()
+        except KeyError:
+            return jsonify({'success': False, 'reason':'Masukan Nama Anda Terlebih Dahulu'})
+        except Exception as exc:
+            return jsonify({'success': False, 'reason':'Exception Caught', 'exception': [exc.__class__.__name__, exc.args]})
+        return jsonify({'success':True, 'success_message':'Kehadiranmu Telah Dicatat Sebagai {}.'.format(data['status'])})
+    elif endpoint.lower() == 'collection':
+        return jsonify({'raw':attendanceList,'str': '\n'.join(['{}. {}, {}'.format(i+1, data[0], data[1]) for i, data in enumerate(attendanceList.items())])})
 
 
 @attendance.route('/')
 def index():
-    return render_template('index.html', env=env)
+    return render_template('index.html', env=env, title="Collective Absence")
+
+
+@attendance.route('/collected')
+def show_collected():
+    copyCandidates = {'Copy all to clipboard':'\n'.join(['{}. {}, {}'.format(i+1, data[0], data[1]) for i, data in enumerate(attendanceList.items())]),
+                      'Copy names to clipboard':'\n'.join(['{}. {}'.format(i+1, name) for i, name in enumerate(attendanceList.keys())])}
+    return render_template('collected.html', env=env, title="Collected Attendance", attendance=attendanceList, copyCandidates=copyCandidates)
+
 
 if __name__ == '__main__':
     from argparse import ArgumentParser
     from flask import Flask
+    
     parser = ArgumentParser(__name__, description="Absen Kolektif")
+    
     parser.add_argument('time_limit', type=timeParser, help='Set the time limit for presence collection')
     parser.add_argument('--load-cached', dest='loadCached', action='store_true', help='Whether to load cached.json or not.')
+    parser.add_argument('--disable-status', dest='statusesToDisable', default='', type=lambda _s:_s.split(','), help="Disable given list of statuses. Format=status1[,status2[,status3....]]")
+
     args = parser.parse_args()
-    if args.loadCached:
-        attendanceList=loadCached()
-        print('Cache Loaded')
+    
     currtime = datetime.now()
     env.time_limit = args.time_limit.replace(year=currtime.year, month=currtime.month, day=currtime.day)
     # print(currtime, env.time_limit)
+
+    if args.loadCached:
+        attendanceList=loadCached()
+        print('Cache Loaded')
+
+    if len(args.statusesToDisable) > 0:
+        env.disabled_statuses = ["{}{}".format(s[0].upper(), s[1:].lower()) for s in args.statusesToDisable]
+        print("Disabled Statuses: {}".format(', '.join(env.disabled_statuses)))
+
     
     app = Flask(__name__)
     app.register_blueprint(attendance, url_prefix='/')
     app.run('0.0.0.0', 58082, debug=True)
+    
--- static/script.js
+++ static/script.js
@@ -1,30 +1,78 @@
 var debugging = new Map(); //Keys: response,button
 
-function poster(button) {
+function setStatus(button) {
     debugging.set('button', button);
+    $("#statusPlaceholder").attr("value", button.value);
 
-    $('#4yvftjy34dmt5').remove();
-    $("<input />").attr('id', '4yvftjy34dmt5').attr("type", "hidden").attr("name", "status").attr("value", button.value).appendTo("#absenForm");
 }
 
 $('#absenForm').submit(function(e){
     e.preventDefault();
-    formData = $('#absenForm').serialize();
     $.ajax({
-        url: '/input',
+        url: '/API/input',
         type: 'post',
         data:$('#absenForm').serialize(),
         success:function(retval){
             $('#response-alert').remove();
             if (retval['success']) {
                 var alertObj = `<div id="response-alert" class="alert alert-success" role="alert">${retval['success_message']}</div>`;
-                $('#timeLimit').prepend(alertObj);
+                $('#responsePlaceholder').prepend(alertObj);
             }
             else {
                 var alertObj = `<div id="response-alert" class="alert alert-danger" role="alert">${retval['reason']}</div>`;
-                $('#timeLimit').prepend(alertObj);
+                $('#responsePlaceholder').prepend(alertObj);
             }
             debugging.set('response', retval)
         }
     });
 });
+
+
+// #########################
+// FROM: https://stackoverflow.com/questions/400212/how-do-i-copy-to-the-clipboard-in-javascript
+
+function fallbackCopyTextToClipboard(text) {
+    var textArea = document.createElement("textarea");
+    textArea.value = text;
+    
+    // Avoid scrolling to bottom
+    textArea.style.top = "0";
+    textArea.style.left = "0";
+    textArea.style.position = "fixed";
+  
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
+  
+    try {
+        var successful = document.execCommand('copy');
+        var msg = successful ? 'successful' : 'unsuccessful';
+        debugging.set('clipboardCopying', 'Fallback: Copying text command was ' + msg);
+    } catch (err) {
+        debugging.set('clipboardCopying', 'Fallback: Oops, unable to copy');
+        debugging.set('clipboardCopyingErr', err);
+    }
+    document.body.removeChild(textArea);
+}
+
+function copyTextToClipboard(text) {
+    if (!navigator.clipboard) {
+        fallbackCopyTextToClipboard(text);
+        return;
+    }
+    navigator.clipboard.writeText(text).then(function() {
+        debugging.set('clipboardCopying', 'Async: Copying to clipboard was successful!');
+    }, function(err) {
+        debugging.set('clipboardCopying', 'Async: Could not copy text:');
+        debugging.set('clipboardCopyingErr', err);
+    });
+}
+
+// #########################
+
+function copyCollectionToClipboard(text) {
+    // Use JS builtin selector, jquery selector returns an array, hence the inability to access the property value
+    // If you want to use jquery, select the item inside the array first, then get its value.
+    let header = document.getElementById('copyToClipboardHeader').value;
+    copyTextToClipboard(header + '\n' + text);
+}
