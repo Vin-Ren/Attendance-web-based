006fa2127665f3997cbff5ba7481e3ce8ce4bae6
vin-ren <65110648+vin-ren@users.noreply.github.com>
2022-02-23 19:40:48 +0900

    Restructure app.py, added waitress support
    Restructured app.py, encapsulate argument parsing and starting web server into their own functions, removing some redundant format for time stripping for time_limit.
    Added support to run the application with waitress.
    Added options to make the application more flexible.
    Added option to run the web server without time limit, added time_limit with prefix +, but only for minutes.

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from faulthandler import disable
+from argparse import ArgumentParser
 import json
 from datetime import datetime
-from flask import Blueprint, jsonify, render_template, url_for, request
+from flask import Blueprint, jsonify, render_template, request
+
 
 dictUpdater = lambda dbase,dupdt:(lambda base,updater:[base.update(updater), base][-1])(dbase.copy(),dupdt)
+
 
 class objectifiedDict(dict):
     def __getattribute__(self, name):
@@ -16,11 +18,8 @@ class objectifiedDict(dict):
         return super().__setitem__(name, value)
 
 
-env = objectifiedDict(time_format='%H:%M', cacheFile='cached.json')
+env = objectifiedDict(time_format='%H:%M', cacheFile='cached.json', time_limited=True)
 attendanceList = {}
-
-
-attendance = Blueprint('attendance', __name__, url_prefix='/attendance')
 
 
 def loadCached():
@@ -35,15 +34,21 @@ def saveAttendance():
     with open(env.cacheFile, 'w') as f:
         json.dump(attendanceList, f, indent=2)
 
-def timeParser(s:str):
-    formats = ['%H:%M', '%H:%M:%S', '%d %b %H:%M', '%H %M', '%H %M %S', '%H']
 
-    for format in formats:
+def timeParser(s:str):
+    formats = ['%H:%M', '%H:%M:%S', '%H %M', '%H %M %S', '%H', '%M']
+    for _format in formats:
         try:
-            return datetime.strptime(s, format)
+            return datetime.strptime(s, _format)
         except ValueError:
+            # print(s, _format)
             pass
     return datetime.fromtimestamp(int(s))
+
+
+
+attendance = Blueprint('attendance', __name__, url_prefix='/attendance')
+
 
 @attendance.route('/API/<string:endpoint>', methods=['GET', 'POST'])
 def API(endpoint):
@@ -80,20 +85,43 @@ def show_collected():
     return render_template('collected.html', env=env, title="Collected Attendance", attendance=attendanceList, copyCandidates=copyCandidates)
 
 
-if __name__ == '__main__':
-    from argparse import ArgumentParser
-    from flask import Flask
-    
+def parseArguments():
     parser = ArgumentParser(__name__, description="Absen Kolektif")
     
-    parser.add_argument('time_limit', type=timeParser, help='Set the time limit for presence collection')
+    parser.add_argument('-t', '--time_limit', dest='time_limit', metavar='TimeLimit', default='0', help='Set the time limit for presence collection')
     parser.add_argument('--load-cached', dest='loadCached', action='store_true', help='Whether to load cached.json or not.')
-    parser.add_argument('--disable-status', dest='statusesToDisable', default='', type=lambda _s:_s.split(','), help="Disable given list of statuses. Format=status1[,status2[,status3....]]")
+    parser.add_argument('--disable-status', metavar='STATUSES_TO_DISABLE', dest='statusesToDisable', default='', type=lambda _s:_s.split(','), help="Disable given list of statuses. Format=status1[,status2[,status3....]]")
+    parser.add_argument('--host', dest='host', default='0.0.0.0', help="Host to run the web server on. Default='0.0.0.0' AKA Localhost")
+    parser.add_argument('--port', dest='port', default=58082, type=int, help="Port to run the web server on. Default=58082")
+    parser.add_argument('-w', '--waitress', dest='use_waitress', action='store_true', help='Run the web server using waitress')
+    parser.add_argument('--threads', dest='threads', default=__import__('os').cpu_count(), help='Threads to run waitress on. Default={}'.format(__import__('os').cpu_count()))
+    parser.add_argument('-d', '--debug', dest='debug', action='store_true', help='Starts Web server in Debug')
 
-    args = parser.parse_args()
+    return parser.parse_args()
+
+
+def main():
+    from flask import Flask
     
-    currtime = datetime.now()
-    env.time_limit = args.time_limit.replace(year=currtime.year, month=currtime.month, day=currtime.day)
+    app = Flask(__name__)
+    app.register_blueprint(attendance, url_prefix='/')
+    
+    args = parseArguments()
+
+    # Processing args values
+
+    if args.time_limit=='0':
+        print('Running without time limit')
+        env.time_limited = False
+        env.time_limit = datetime.fromtimestamp(0)
+    else:
+        currtime = datetime.now() # make it accept something like +00:30 as until 30 minutes after now
+        time_limit = timeParser(args.time_limit.strip('+'))
+        if args.time_limit.startswith('+'): # assume everything passed as in minutes
+            # plus_seconds_delta = time_limit.replace(year=1970,month=1,day=1) - datetime.fromtimestamp(0) # This doesn't work
+            env.time_limit = datetime.fromtimestamp(currtime.timestamp()+int(args.time_limit.strip('+'))*60)
+        else:
+            env.time_limit = time_limit.replace(year=currtime.year, month=currtime.month, day=currtime.day)
     # print(currtime, env.time_limit)
 
     if args.loadCached:
@@ -104,8 +132,12 @@ if __name__ == '__main__':
         env.disabled_statuses = ["{}{}".format(s[0].upper(), s[1:].lower()) for s in args.statusesToDisable]
         print("Disabled Statuses: {}".format(', '.join(env.disabled_statuses)))
 
+    if args.use_waitress:
+        import waitress
+        waitress.serve(app=app, host=args.host, port=args.port, threads=args.threads)
+    else:
+        app.run(host=args.host, port=args.port, debug=args.debug)
     
-    app = Flask(__name__)
-    app.register_blueprint(attendance, url_prefix='/')
-    app.run('0.0.0.0', 58082, debug=True)
-    
+
+if __name__ == '__main__':
+    main()
